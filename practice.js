9e555d106b17932c4be4c3bd844516f68bb5aea1
pravingavani66 <pravingavani66@gmail.com>
2021-01-17 20:15:59 +0530

    practicing javascript

--- practice.js
+++ practice.js
@@ -9,3 +9,18 @@ const scope3 = "value can't change"
  console.log("let keyword variable scope:" + scope);
  console.log("var keyword variable scope:" + scope2);
  console.log("const keyword variable:" + scope3);
+
+ //type of variables
+//  let name ="'Hello world";
+
+//  console.log(name);
+// let message = `Hello World!`;
+// console.log(message);
+// let name ="Adrita";
+// let message = `Hello ${name}!`;
+// console.log(message);
+// Give an example with New Line as well.
+// let message = `Hello
+// ${name}!`;
+// console.log(message);
+ 
