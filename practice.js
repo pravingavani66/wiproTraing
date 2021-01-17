cc61b1940c9812301c673892d036570f87b1280d
pravingavani66 <pravingavani66@gmail.com>
2021-01-17 20:04:33 +0530

    adding variable

--- practice.js
+++ practice.js
@@ -1,2 +1,11 @@
 //alert("Hello World from JS");
 console.log("Hello World from JS");
+
+
+//variables
+let scope ='block level';
+var scope2 = 'local variable';
+const scope3 = "value can't change"
+ console.log("let keyword variable scope:" + scope);
+ console.log("var keyword variable scope:" + scope2);
+ console.log("const keyword variable:" + scope3);
